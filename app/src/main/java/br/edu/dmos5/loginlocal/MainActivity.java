1073354403fc91fd0392231abed03499e73cf267
Thiago <61062313+ThiagoCav@users.noreply.github.com>
2020-08-21 22:41:55 -0300

    Commit_4

--- app/src/main/java/br/edu/dmos5/loginlocal/MainActivity.java
+++ app/src/main/java/br/edu/dmos5/loginlocal/MainActivity.java
@@ -1,16 +1,27 @@
 package br.edu.dmos5.loginlocal;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent; import android.content.SharedPreferences;
-import android.os.Bundle; import android.util.Log; import android.view.View; import android.widget.Button; import android.widget.CheckBox; import android.widget.EditText; import android.widget.TextView; import android.widget.Toast;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //Objetos utilizados para armazenar e recuperar os dados
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
     private EditText usuarioEditText;
     private EditText senhaEditText;
     private Button logarButton;
     private CheckBox lembrarCheckBox;
     private TextView novoUsuarioTextView;
-    private String usuario,senha;
+    private String usuario, senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         lembrarCheckBox = findViewById(R.id.checkbox_lembrar);
         novoUsuarioTextView = findViewById(R.id.textview_novo);
         logarButton.setOnClickListener(this);
+        novoUsuarioTextView.setOnClickListener(this);
+        //Vamos instanciar as preferencias em modo privado, ou seja, somente acessíveis ao próprio app.
+
+        mSharedPreferences = this.getPreferences(MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
     }
 
     @Override
@@ -37,9 +53,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onRestart();
     }
 
+    /*
+    No onResumo() é um bom momento para verificar se o usuário possui dados armazenados ou não,     lembre-se que o onCreate() só é executado uma vez.
+     */
     @Override
     protected void onResume() {
         Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onResume()");
+
+        //Vamos verificar se o usuário possui preferências
+        verificarPreferencias();
+
         super.onResume();
     }
 
@@ -70,22 +93,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(this, R.string.erro_entrada_msg, Toast.LENGTH_SHORT).show();
                 return;
             }
-
+            //Antes de abrir a outra tela se verifica se o usuário deseja armazenar
+            //os dados de login para outros acessos.
+            salvaPreferencias();
             abrirBoasVindas();
             return;
         }
-
     }
 
     private void abrirBoasVindas() {
         Intent in = new Intent(this, BemVindoActivity.class);
-
-
-    Bundle args = new Bundle();
+        Bundle args = new Bundle();
         args.putString(getString(R.string.key_usuario), usuario);
         args.putString(getString(R.string.key_senha), senha);
-                in.putExtras(args);
-                startActivity(in);
+        in.putExtras(args);
+        startActivity(in);
     }
 
+
+    private void salvaPreferencias() {
+
+        if (lembrarCheckBox.isChecked()) {
+            mEditor.putString(getString(R.string.key_usuario), usuario);
+            mEditor.commit();
+            mEditor.putString(getString(R.string.key_senha), senha);
+            mEditor.commit();
+            mEditor.putBoolean(getString(R.string.key_lembrar), true);
+            mEditor.commit();
+        } else {
+            mEditor.putString(getString(R.string.key_usuario), "");
+            mEditor.commit();
+            mEditor.putString(getString(R.string.key_senha), "");
+            mEditor.commit();
+            mEditor.putBoolean(getString(R.string.key_lembrar), false);
+            mEditor.commit();
+        }
+    }
+
+    /*
+        Aqui recuperamos as preferências do usuário, e caso existam (boolean lembrar) atualizamos     os dados na tela da activity.
+         */
+    private void verificarPreferencias() {
+        usuario = mSharedPreferences.getString(getString(R.string.key_usuario), "");
+        senha = mSharedPreferences.getString(getString(R.string.key_senha), "");
+        boolean lembrar = mSharedPreferences.getBoolean(getString(R.string.key_lembrar), false);
+        if (lembrar) {
+            usuarioEditText.setText(usuario);
+            senhaEditText.setText(senha);
+            lembrarCheckBox.setChecked(true);
+        }
+    }
 }
