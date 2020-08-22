1b3b9e71bf3f6fbcd2132ff3487fde7083097753
Thiago <61062313+ThiagoCav@users.noreply.github.com>
2020-08-21 23:09:38 -0300

    Commit_5

--- app/src/main/java/br/edu/dmos5/loginlocal/BemVindoActivity.java
+++ app/src/main/java/br/edu/dmos5/loginlocal/BemVindoActivity.java
@@ -3,80 +3,115 @@ package br.edu.dmos5.loginlocal;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BemVindoActivity extends AppCompatActivity {
     private TextView mensagemTextView;
     private String usuario;
     private String senha;
+    private User mUser;
+    private List<User> userList = null;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onCreate()");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bem_vindo);
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onCreate()");
+        super.onCreate(savedInstanceState);         setContentView(R.layout.activity_bem_vindo);
         mensagemTextView = findViewById(R.id.textview_mensagem);
         //Apresenta navigation up
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Intent intent = getIntent();
         usuario = intent.getStringExtra(getString(R.string.key_usuario));
         senha = intent.getStringExtra(getString(R.string.key_senha));
+        mSharedPreferences = this.getSharedPreferences(getString(R.string.file_usuario),
+                MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
+        recuperaUsuarios();
         validarUsuario();
     }
-
     @Override
     protected void onStart() {
         Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onStart()");
         super.onStart();
     }
-
     @Override
     protected void onRestart() {
         Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onRestart()");
         super.onRestart();
     }
-
     @Override
     protected void onResume() {
         Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onResume()");
         super.onResume();
     }
-
     @Override
     protected void onPause() {
         Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onPause()");
         super.onPause();
     }
-
     @Override
     protected void onStop() {
         Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onStop()");
         super.onStop();
     }
-
     @Override
     protected void onDestroy() {
         Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onDestroy()");
         super.onDestroy();
     }
-
     private void validarUsuario(){
-        //Observe que a validação se dá com os dados armazenados no arquivo de recursos.
-        if(usuario.equals(getString(R.string.user_default)) && senha.equals(getString(R.string.passwd_default))) {
-            mensagemTextView.setText(R.string.bem_vindo_msg);
-    }else{
-        mensagemTextView.setText(R.string.erro_login_msg);
+
+        boolean achou = false;
+        if(userList != null) {
+            for (User u : userList) {
+            if (u.getUsername().equals(usuario)) {
+                if (u.getPassword().equals(senha)) {
+                mensagemTextView.setText(R.string.bem_vindo_msg);
+                achou = true;
+            }
+            }
+        }
+        }         if(!achou){
+            mensagemTextView.setText(R.string.erro_login_msg);
+        }
     }
-}
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
+        if(item.getItemId() == android.R.id.home){
             finish();
         }
         return super.onOptionsItemSelected(item);
     }
-
+    private void recuperaUsuarios(){
+        String users = mSharedPreferences.getString(getString(R.string.key_usuarios_db), "");
+        JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray;
+        userList = new ArrayList<>();
+        try{
+            jsonArray = new JSONArray(users);
+            for(int i=0; i<jsonArray.length(); i++){
+                jsonObject = jsonArray.getJSONObject(i);
+                mUser = new User(jsonObject.getString("username"), jsonObject.getString("password"));
+                userList.add(mUser);
+            }
+        } catch (JSONException ex){
+            userList = null;
+        }
+        if (userList != null) {
+            for (User u : userList) {
+            Log.i(getString(R.string.tag), u.toString());
+        }
+        }
+    }
 }
+
--- app/src/main/java/br/edu/dmos5/loginlocal/MainActivity.java
+++ app/src/main/java/br/edu/dmos5/loginlocal/MainActivity.java
@@ -25,9 +25,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onCreate()");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onCreate()");
+        super.onCreate(savedInstanceState);         setContentView(R.layout.activity_main);
         usuarioEditText = findViewById(R.id.edittext_usuario);
         senhaEditText = findViewById(R.id.edittext_senha);
         logarButton = findViewById(R.id.button_logar);
@@ -35,73 +34,70 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         novoUsuarioTextView = findViewById(R.id.textview_novo);
         logarButton.setOnClickListener(this);
         novoUsuarioTextView.setOnClickListener(this);
+
         //Vamos instanciar as preferencias em modo privado, ou seja, somente acessíveis ao próprio app.
 
         mSharedPreferences = this.getPreferences(MODE_PRIVATE);
+        mSharedPreferences = this.getSharedPreferences(getString(R.string.file_preferences),
+        MODE_PRIVATE);
         mEditor = mSharedPreferences.edit();
     }
-
     @Override
     protected void onStart() {
-        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onStart()");
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onStart()");
         super.onStart();
     }
-
     @Override
     protected void onRestart() {
-        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onRestart()");
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onRestart()");
         super.onRestart();
     }
-
     /*
     No onResumo() é um bom momento para verificar se o usuário possui dados armazenados ou não,     lembre-se que o onCreate() só é executado uma vez.
      */
     @Override
     protected void onResume() {
-        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onResume()");
-
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onResume()");
         //Vamos verificar se o usuário possui preferências
         verificarPreferencias();
-
         super.onResume();
     }
-
     @Override
     protected void onPause() {
-        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onPause()");
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onPause()");
         super.onPause();
     }
-
     @Override
     protected void onStop() {
-        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onStop()");
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onStop()");
         super.onStop();
     }
-
     @Override
     protected void onDestroy() {
-        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onDestroy()");
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onDestroy()");
         super.onDestroy();
     }
-
     @Override
     public void onClick(View view) {
-        if (view == logarButton) {
-            usuario = usuarioEditText.getText().toString();
-            senha = senhaEditText.getText().toString();
-            if (usuario.isEmpty() || senha.isEmpty()) {
-                Toast.makeText(this, R.string.erro_entrada_msg, Toast.LENGTH_SHORT).show();
-                return;
-            }
-            //Antes de abrir a outra tela se verifica se o usuário deseja armazenar
-            //os dados de login para outros acessos.
-            salvaPreferencias();
-            abrirBoasVindas();
+        if(view == logarButton){
+        usuario = usuarioEditText.getText().toString();
+        senha = senhaEditText.getText().toString();
+        if(usuario.isEmpty() || senha.isEmpty()){
+            Toast.makeText(this, R.string.erro_entrada_msg, Toast.LENGTH_SHORT).show();
             return;
         }
+        //Antes de abrir a outra tela se verifica se o usuário deseja armazenar
+        //os dados de login para outros acessos.
+            salvaPreferencias();
+            abrirBoasVindas();
+        return;
     }
-
-    private void abrirBoasVindas() {
+        if(view == novoUsuarioTextView){
+            Intent in = new Intent(this, NovoUsuarioActivity.class);
+            startActivity(in);             return;
+        }
+    }
+    private void abrirBoasVindas(){
         Intent in = new Intent(this, BemVindoActivity.class);
         Bundle args = new Bundle();
         args.putString(getString(R.string.key_usuario), usuario);
@@ -109,35 +105,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         in.putExtras(args);
         startActivity(in);
     }
-
-
-    private void salvaPreferencias() {
-
-        if (lembrarCheckBox.isChecked()) {
-            mEditor.putString(getString(R.string.key_usuario), usuario);
-            mEditor.commit();
-            mEditor.putString(getString(R.string.key_senha), senha);
-            mEditor.commit();
-            mEditor.putBoolean(getString(R.string.key_lembrar), true);
-            mEditor.commit();
-        } else {
-            mEditor.putString(getString(R.string.key_usuario), "");
-            mEditor.commit();
-            mEditor.putString(getString(R.string.key_senha), "");
-            mEditor.commit();
-            mEditor.putBoolean(getString(R.string.key_lembrar), false);
-            mEditor.commit();
-        }
+    private void salvaPreferencias(){
+        //Caso o checkbox esteja marcado, armazenamos os dados no objeto,caso contrário vamos apenas armazenar um vazio.
+        if(lembrarCheckBox.isChecked()){
+        mEditor.putString(getString(R.string.key_usuario), usuario);
+        mEditor.commit();
+        mEditor.putString(getString(R.string.key_senha), senha);
+        mEditor.commit();
+        mEditor.putBoolean(getString(R.string.key_lembrar), true);
+        mEditor.commit();
+    }else{
+        mEditor.putString(getString(R.string.key_usuario), "");
+        mEditor.commit();
+        mEditor.putString(getString(R.string.key_senha), "");
+        mEditor.commit();
+        mEditor.putBoolean(getString(R.string.key_lembrar), false);
+        mEditor.commit();
     }
-
+    }
     /*
-        Aqui recuperamos as preferências do usuário, e caso existam (boolean lembrar) atualizamos     os dados na tela da activity.
-         */
+    Aqui recuperamos as preferências do usuário, e caso existam (boolean lembrar) atualizamos     os dados na tela da activity.
+     */
     private void verificarPreferencias() {
         usuario = mSharedPreferences.getString(getString(R.string.key_usuario), "");
         senha = mSharedPreferences.getString(getString(R.string.key_senha), "");
         boolean lembrar = mSharedPreferences.getBoolean(getString(R.string.key_lembrar), false);
-        if (lembrar) {
+        if(lembrar){
             usuarioEditText.setText(usuario);
             senhaEditText.setText(senha);
             lembrarCheckBox.setChecked(true);
