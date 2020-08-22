a28015a8830913d5b489c55e5a3eea07ae29ff95
Thiago <61062313+ThiagoCav@users.noreply.github.com>
2020-08-21 22:17:01 -0300

    Commit_3

--- app/src/main/java/br/edu/dmos5/loginlocal/BemVindoActivity.java
+++ app/src/main/java/br/edu/dmos5/loginlocal/BemVindoActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+
 public class BemVindoActivity extends AppCompatActivity {
     private TextView mensagemTextView;
     private String usuario;
@@ -14,29 +15,62 @@ public class BemVindoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bem_vindo);
         mensagemTextView = findViewById(R.id.textview_mensagem);
         //Apresenta navigation up
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //Recupera os dados enviados pela MainActivity
         Intent intent = getIntent();
         usuario = intent.getStringExtra(getString(R.string.key_usuario));
         senha = intent.getStringExtra(getString(R.string.key_senha));
-        //Vamos anaisar se o usuário pode logar ou não
         validarUsuario();
     }
 
-    private void validarUsuario() {
-        //Observe que a validação se dá com os dados armazenados no arquivo de recursos.
-        if (usuario.equals(getString(R.string.user_default)) &&
-                senha.equals(getString(R.string.passwd_default))) {
-            mensagemTextView.setText(R.string.bem_vindo_msg);
-        } else {
-            mensagemTextView.setText(R.string.erro_login_msg);
-        }
+    @Override
+    protected void onStart() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onStart()");
+        super.onStart();
     }
 
+    @Override
+    protected void onRestart() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onRestart()");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onResume()");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() +  "| Método : onDestroy()");
+        super.onDestroy();
+    }
+
+    private void validarUsuario(){
+        //Observe que a validação se dá com os dados armazenados no arquivo de recursos.
+        if(usuario.equals(getString(R.string.user_default)) && senha.equals(getString(R.string.passwd_default))) {
+            mensagemTextView.setText(R.string.bem_vindo_msg);
+    }else{
+        mensagemTextView.setText(R.string.erro_login_msg);
+    }
+}
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
@@ -44,4 +78,5 @@ public class BemVindoActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
--- app/src/main/java/br/edu/dmos5/loginlocal/MainActivity.java
+++ app/src/main/java/br/edu/dmos5/loginlocal/MainActivity.java
@@ -1,16 +1,9 @@
 package br.edu.dmos5.loginlocal;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.content.Intent; import android.content.SharedPreferences;
+import android.os.Bundle; import android.util.Log; import android.view.View; import android.widget.Button; import android.widget.CheckBox; import android.widget.EditText; import android.widget.TextView; import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText usuarioEditText;
     private EditText senhaEditText;
@@ -18,8 +11,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private CheckBox lembrarCheckBox;
     private TextView novoUsuarioTextView;
     private String usuario,senha;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         usuarioEditText = findViewById(R.id.edittext_usuario);
@@ -29,25 +24,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         novoUsuarioTextView = findViewById(R.id.textview_novo);
         logarButton.setOnClickListener(this);
     }
+
+    @Override
+    protected void onStart() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onRestart()");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onResume()");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onDestroy()");
+        super.onDestroy();
+    }
+
     @Override
     public void onClick(View view) {
-        if(view == logarButton){
+        if (view == logarButton) {
             usuario = usuarioEditText.getText().toString();
             senha = senhaEditText.getText().toString();
-            if(usuario.isEmpty() || senha.isEmpty()){
+            if (usuario.isEmpty() || senha.isEmpty()) {
                 Toast.makeText(this, R.string.erro_entrada_msg, Toast.LENGTH_SHORT).show();
                 return;
             }
+
             abrirBoasVindas();
             return;
         }
+
     }
-    private void abrirBoasVindas(){
+
+    private void abrirBoasVindas() {
         Intent in = new Intent(this, BemVindoActivity.class);
-        Bundle args = new Bundle();
+
+
+    Bundle args = new Bundle();
         args.putString(getString(R.string.key_usuario), usuario);
         args.putString(getString(R.string.key_senha), senha);
-        in.putExtras(args);
-        startActivity(in);
+                in.putExtras(args);
+                startActivity(in);
     }
+
 }
